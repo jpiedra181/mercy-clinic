35b18727e335c382797dfe53da8c6396d6d22897
JPiedra <javierpiedraiglesias@gmail.com>
2025-05-31 15:25:09 +0200

    updating base and homepage values

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/mercy-clinic/",
+    base: "/mercy-clinic",
     plugins: [react(), tailwindcss()],
 })
